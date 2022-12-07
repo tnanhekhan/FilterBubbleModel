bb7a816afc7474f72213256c443127ac547f15cd
Tabish Nanhekhan <1631995@gmail.com>
2022-12-07 10:09:42 +0100

    local d3

--- visualization/modules/XYScaledNetworkModule.py
+++ visualization/modules/XYScaledNetworkModule.py
@@ -6,14 +6,14 @@ Module for rendering the network, using [d3.js](https://d3js.org/) frameworks.
 """
 from mesa.visualization.ModularVisualization import VisualizationElement
 
+
 class XYScaledNetworkModule(VisualizationElement):
     package_includes = []
 
     def __init__(
-        self, portrayal_method, canvas_height=500, canvas_width=500):
-
-        self.package_includes = ["d3.min.js"]
-        self.local_includes = ["./visualization/templates/js/XYScaledNetworkModule_d3.js"]
+            self, portrayal_method, canvas_height=500, canvas_width=500):
+        self.local_includes = ["./visualization/templates/js/d3.min.js",
+                               "./visualization/templates/js/XYScaledNetworkModule_d3.js"]
 
         self.portrayal_method = portrayal_method
         self.canvas_height = canvas_height
